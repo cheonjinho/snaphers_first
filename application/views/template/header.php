432acefc09b1a6cba245eb83b34f809b16fd5b0f
jin <sr_soou@naver.com>
2017-04-18 00:57:22 +0900

    add stylesheet

---
+++ application/views/template/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>SNAPHERS</title>
+	<link rel="stylesheet" href="/assets/stylesheet/application.css" type="text/css"/>
+</head>
+<body>
+Hello Snaphers header.
+</body>
+</html>
