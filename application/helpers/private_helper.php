c78350870961186447581c4c85379ca5515f409f
jin <sr_soou@naver.com>
2017-04-18 00:28:40 +0900

    initial setting before start project

---
+++ application/helpers/private_helper.php
@@ -0,0 +1,25 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed'); 
+
+function database_username(){
+	return 'root';
+}
+
+function database_hostname(){
+	return 'php.cqk456tmxvqs.ap-northeast-2.rds.amazonaws.com';
+}
+
+function database_password(){
+	return 'wlsghcjs';
+}
+
+function database_database(){
+	return 'Php_Test';
+}
+
+function aws_key(){
+	return 'AKIAJZ7Q6YBOY25WL7JA';
+}
+
+function aws_secret(){
+	return '5fQYU8De6UOXG0skAPZhwpTKAbEfs7RzssUw4Ncm';
+}
